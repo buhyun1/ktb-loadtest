168e35e4a1204d85e31033e45abb003d1290c059
buhyun <buhyun8@pukyong.ac.kr>
2025-07-25 02:38:54 +0900

    백엔드 유저 이미지 s3 env 수정

--- Backend/services/s3Service.js
+++ Backend/services/s3Service.js
@@ -1,16 +1,16 @@
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.S3_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
   },
 });
 
 exports.uploadFile = async (fileBuffer, fileName, mimeType) => {
   const params = {
-    Bucket: process.env.AWS_S3_BUCKET,
+    Bucket: process.env.S3_BUCKET,
     Key: fileName,
     Body: fileBuffer,
     ContentType: mimeType,
